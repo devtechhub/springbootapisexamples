e8707de1dc5fc79aa23b394fca66fccf82f48b15
lakhmapurkar.vishal@gmail.com <Vish$9987>
2021-09-26 23:23:29 +0200

    added first commit

---
+++ src/main/java/com/devtechhub/springbootapisexamples/SpringbootapisexamplesApplication.java
@@ -0,0 +1,13 @@
+package com.devtechhub.springbootapisexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootapisexamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootapisexamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devtechhub/springbootapisexamples/SpringbootapisexamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtechhub.springbootapisexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootapisexamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
